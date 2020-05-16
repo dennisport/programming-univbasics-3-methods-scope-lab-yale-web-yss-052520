19b30045f3cea7155fd07da11d96dbc20e3d90d1
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-16 21:55:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,16 @@ def mario
 end
 
 def toadstool
-  
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end 
+
+def all_phrases($name)
+  puts #{$name}
+end 
+  
